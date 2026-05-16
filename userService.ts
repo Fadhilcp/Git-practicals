3bd01cc98207b89025a07e309ee216efd2879360
Fadhilcp <fadhilrahamancph@gmail.com>
2026-05-16 09:38:00 +0530

    update user-service

--- userService.ts
+++ userService.ts
@@ -9,4 +9,20 @@ class userService {
     constructor(user: User | null){
         this.user = user
     }
+
+    get email(){
+        console.log('Loading...');
+        return `Email is ${this.user?.email}`;
+    }
+    
+    get name(){
+        setTimeout(() => {
+            console.log('Loading...');
+        }, 2000);
+        return `Name is ${this.user?.name}`;
+    }
+
+    get role(){
+        return this.user?.role;
+    };
 }
